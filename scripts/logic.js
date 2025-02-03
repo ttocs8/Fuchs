de4362809758bb512b6a6add08a7c34741822edb
Scott Hunt <2022hunt.sn@gmail.com>
2025-02-03 01:51:39 -0500

    Backend overhaul + sort button

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,129 +1,109 @@
+const CARDINFOMAP = new Map();
+CARDINFOMAP.set("eichel_Ober", [1,4]);
+CARDINFOMAP.set("grune_Ober", [2,3]);
+CARDINFOMAP.set("eichel_Bauer", [3,2]);
+CARDINFOMAP.set("grune_Bauer", [4,2]);
+CARDINFOMAP.set("rote_Bauer", [5,2]);
+CARDINFOMAP.set("schelle_Bauer", [6,2]);
+CARDINFOMAP.set("rote_Ass", [7,11]);
+CARDINFOMAP.set("rote_Zehner", [8,10]);
+CARDINFOMAP.set("rote_Koenig", [9,4]);
+CARDINFOMAP.set("rote_Ober", [10,3]);
+CARDINFOMAP.set("rote_Neuner", [11,0]);
+CARDINFOMAP.set("rote_Achter", [12,0]);
+CARDINFOMAP.set("eichel_Ass", [13,11]);
+CARDINFOMAP.set("eichel_Zehner", [14,10]);
+CARDINFOMAP.set("eichel_Koenig", [15,4]);
+CARDINFOMAP.set("eichel_Neuner", [16,0]);
+CARDINFOMAP.set("grune_Ass", [17,11]);
+CARDINFOMAP.set("grune_Zehner", [18,10]);
+CARDINFOMAP.set("grune_Koenig", [19,4]);
+CARDINFOMAP.set("grune_Neuner", [20,0]);
+CARDINFOMAP.set("schelle_Ass", [21,11]);
+CARDINFOMAP.set("schelle_Zehner", [22,10]);
+CARDINFOMAP.set("schelle_Koenig", [23,4]);
+CARDINFOMAP.set("schelle_Ober", [24,3]);
+
 class Card
 {
 	order = -1;
-	constructor(imgsrc, suit, rank, value, isTrump) {
-		this.imgsrc = imgsrc;
+	value = -1;
+	imgsrc = "undefined";
+	constructor(suit, rank, value, isTrump) {
+		
 		this.suit = suit;
 		this.rank = rank;
 		this.value = value;
+		this.imgsrc = "images/cards/" + suit + "/" + rank + ".png";
+		this.order = CARDINFOMAP.get(this.suit + "_" + this.rank)[0];
 		this.isTrump = isTrump;
-		this.order = ORDER.get(this.suit + this.rank);
 	}
 
 	getImgSrc()
 	{
 		return this.imgsrc;
 	}
-
-	getCardOrder()
-	{
-		return this.order;
-	}
 }
 
-const ORDER = new Map();
-ORDER.set("eichelOber", 1);
-ORDER.set("gruneOber", 2);
-ORDER.set("eichelBauer", 3);
-ORDER.set("gruneBauer", 4);
-ORDER.set("roteBauer", 5);
-ORDER.set("schelleBauer", 6);
-ORDER.set("roteAss", 7);
-ORDER.set("roteZehner", 8);
-ORDER.set("roteKoenig", 9);
-ORDER.set("roteOber", 10);
-ORDER.set("roteNeuner", 11);
-ORDER.set("roteAchter", 12);
-ORDER.set("eichelAss", 13);
-ORDER.set("eichelZehner", 14);
-ORDER.set("eichelKoenig", 15);
-ORDER.set("eichelNeuner", 16);
-ORDER.set("gruneAss", 17);
-ORDER.set("gruneZehner", 18);
-ORDER.set("gruneKoenig", 19);
-ORDER.set("gruneNeuner", 20);
-ORDER.set("schelleAss", 21);
-ORDER.set("schelleZehner", 22);
-ORDER.set("schelleKoenig", 23);
-ORDER.set("schelleOber", 24);
-
-const SuitsArray = ["eichel","grune","rote","schelle"];
-const RanksArray = ["Ass","Zehner","Koenig","Ober","Bauer","Neuner","Achter"];
-
-function DealCards(){
-	const hand = []; 
-
-	for(var i = 0; i < 6; i++){
-		//pick suit
-		var randSuitNum = Math.floor(Math.random()*SuitsArray.length);
-		var suit = SuitsArray[randSuitNum];
-		//console.log(suit);
-
-		//pick rank
-		var randRankNum = 0;
-		if(suit.includes("rote"))
-			randRankNum = Math.floor(Math.random()*RanksArray.length);
-		else if(suit.includes("grune"))
-			randRankNum = Math.floor(Math.random()* (RanksArray.length-2));
-		else 
-			randRankNum = Math.floor(Math.random()* (RanksArray.length-1));
-		
-		var rank = RanksArray[randRankNum];
-		//console.log(rank);
-
-		var imgSrc = "images/cards/" + suit + "/" + rank + ".png";
-		var card = new Card(imgSrc,suit,rank,-1,false);
-		
-		hand.push(card);
-		//console.log(card.getImgSrc());
+function Deck()
+{
+	this.cards = [];
+	for (let [key, value] of CARDINFOMAP) {
+	
+		suit = key.substring(0, key.indexOf("_"));
+		rank = key.split('_').pop();
+		val = value[1];
+		this.cards.push(new Card(suit,rank,val));
 	}
-	
-	
+		
+	this.shuffle = function(){
+		var j, x, i;
+		for (i = this.cards.length - 1; i > 0; i--) {
+			j = Math.floor(Math.random() * (i + 1));
+			x = this.cards[i];
+			this.cards[i] = this.cards[j];
+			this.cards[j] = x;
+		}
+	}
 
-	return hand;
-	
+	this.getCards = function(number) {
+		if (typeof number === 'undefined') number = 1;
+		var returnCards = [];
+		for (var i = number; i > 0; i--) {
+		  returnCards.push(this.cards.pop());
+		}
+		return returnCards;
+	  }
 }
 
-function hasDuplicates(a) {
-	return _.uniq(a).length !== a.length; 
-  }
 
 $(document).ready(function(){
-	var Hand = DealCards();
 	
-	while(hasDuplicates(Hand)){
-		Hand = DealCards();
-	}
+	var deck = new Deck();
+	deck.shuffle();
+	var Hand = deck.getCards(6);
 	
 	//set hand imgs to respective drawn values from Hand
 	for(var i = 0; i < 6; i++){
 		var cardNum = "#card";
 		cardNum += i+1;
-		$(cardNum).prop("src", Hand[i].getImgSrc());
+		$(cardNum).prop("src", Hand[i].imgsrc);
 	}
 
-	$(document).on('keypress',function(e) {
-		
-			//R - sort cards
-            if( e.which == 114 ) {
-				//sort
-				Hand.sort(function(a, b) {
-					return a.order - b.order;
-				  });
-				  
-				  //redraw cards
-				  for(var i = 0; i < 6; i++){
-					var cardNum = "#card";
-					cardNum += i+1;
-					//console.log(cardNum);
-					//console.log(Hand[i].getImgSrc());
-					$(cardNum).prop("src", Hand[i].getImgSrc());
-				}
-			
-			}
+	$("#sortButton").click(function(){		
+		//sort
+		Hand.sort(function(a, b) {
+			return a.order - b.order;
 		});
-	
 
+		for(var i = 0; i < 6; i++){
+			var cardNum = "#card";
+			cardNum += i+1;
+			$(cardNum).prop("src", Hand[i].imgsrc);
+		}
+	});
+
+	
 });
 	
 
